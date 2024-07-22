5eba2fea1f2cbd4bc607d1211de3b07d70494665
Eo Soo Kim <eosookim@gmail.com>
2024-07-22 15:49:04 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import logo from "../src/assets/images/logo.png";
+
+function App() {
+
+  return (
+    <>
+      <img src={logo}></img>
+    </>
+  )
+}
+
+export default App

e5a86c50e2ec9a0d1c883125fb99e1cf546cea6f
Eo Soo Kim <eosookim@gmail.com>
2024-08-02 23:29:06 -0400

    adds dropdown menu for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,80 @@
 import "./Header.scss";
-import logo from "../../assets/images/logo.png";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import burger from "../../assets/images/burger-bar.png";
+import logo from "../../assets/images/logo.png";
+import arrowDown from '../../assets/images/down-arrow.png';
 
 export default function Header() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [showMenu, setShowMenu] = useState(false);
 
-    const toPage = (page) => {
-        navigate('/' + page);
-    }
+  const toPage = (page) => {
+    navigate("/" + page);
+    setShowMenu(false);
+  };
 
   return (
     <header className="header">
-      <img className="header__logo" src={logo} alt="Cilesia Logo" />
-      <nav className="header__nav">
-        <ul className="menu">
-          <li className="menu__item" onClick={() => toPage("")}>Home</li>
-          <li className="menu__item" onClick={() => toPage("questions")}>FAQ</li>
-          <li className="menu__item" onClick={() => toPage("services")}>Services</li>
-          <li className="menu__item" onClick={() => toPage("book")}>Book</li>
-          <li className="menu__item" onClick={() => toPage("contact")}>Contact</li>
+      <div className="header-bar">
+        <img
+          className="header__logo"
+          src={logo}
+          alt="Cilesia Logo"
+          onClick={() => toPage("")}
+        />
+        <nav className="header__nav">
+          <img
+            className="menu-burger"
+            src={burger}
+            onClick={() => setShowMenu(!showMenu)}
+          ></img>
+          <ul className="menu">
+            <li className="menu__item" onClick={() => toPage("")}>
+              Home
+            </li>
+            <li className="menu__item" onClick={() => toPage("questions")}>
+              FAQ
+            </li>
+            <li className="menu__item" onClick={() => toPage("services")}>
+              Services
+            </li>
+            <li className="menu__item" onClick={() => toPage("book")}>
+              <img></img>
+              Book
+            </li>
+            <li className="menu__item" onClick={() => toPage("contact")}>
+              Contact
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <div className={showMenu ? "dropdown-menu" : "hide-menu"}>
+        <ul className="dropdown-menu__list">
+          <li className="dropdown-menu__item" onClick={() => toPage("")}>
+            Home
+          </li>
+          <li
+            className="dropdown-menu__item"
+            onClick={() => toPage("questions")}
+          >
+            FAQ
+          </li>
+          <li
+            className="dropdown-menu__item"
+            onClick={() => toPage("services")}
+          >
+            Services
+          </li>
+          <li className="dropdown-menu__item" onClick={() => toPage("book")}>
+            Book
+          </li>
+          <li className="dropdown-menu__item" onClick={() => toPage("contact")}>
+            Contact
+          </li>
         </ul>
-      </nav>
+      </div>
     </header>
   );
 }

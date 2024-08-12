a98e46c215eae1b24a318a844bb791da4ffcc1ba
Eo Soo Kim <eosookim@gmail.com>
2024-08-11 23:54:00 -0400

    script now properly loads on book page

--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -1,17 +1,35 @@
 import "./Book.scss";
 import ToBooking from "../../components/ToBooking/ToBooking";
 import { useState, useEffect } from "react";
+// import { postscribe } from "postscribe";
 
 export default function Book() {
   const [book, setBook] = useState(false);
+  const [script, setScript] = useState(null);
+  // useEffect(() => {
+  //   const script = document.createElement("script");
 
-    useEffect(() => {
-      const script = document.createElement('script');
+  //   script.src =
+  //     "https://square.site/appointments/buyer/widget/vp26zekhc1y452/L91FW9Z6QEKDK.js";
+  //   script.async = false;
 
-      script.src = "https://square.site/appointments/buyer/widget/vp26zekhc1y452/L91FW9Z6QEKDK.js";
-      script.async = true;
-      
-      document.body.appendChild(script);
+  //   var body = document.getElementsByTagName("main");
+  //   body.appendChild(script);
+
+  //   return () => {
+  //     body.removeChild(script);
+  //   };
+  // }, []);
+  useEffect(() => {
+    var loadScript = function(src){
+      var script = document.createElement('script');
+      script.async = false;
+      script.src = src;
+      var body = document.getElementsByTagName('main')[0];
+      body.appendChild(script);
+    }
+
+    loadScript('https://square.site/appointments/buyer/widget/vp26zekhc1y452/L91FW9Z6QEKDK.js');
   }, []);
 
   useEffect(() => {
@@ -19,14 +37,6 @@ export default function Book() {
   }, []);
   return (
     <main className="book">
-      <script src='https://square.site/appointments/buyer/widget/vp26zekhc1y452/L91FW9Z6QEKDK.js'></script>
-      {book ? (
-        <ToBooking
-          url={
-            "https://square.site/appointments/buyer/widget/vp26zekhc1y452/L91FW9Z6QEKDK.js"
-          }
-        />
-      ) : null}
       <div className="book__hero">
         <h1 className="book__title">Book</h1>
         <p className="book__body">
--- src/scripts/useScript.jsx
+++ src/scripts/useScript.jsx
@@ -7,10 +7,11 @@ const useScript = url => {
         script.src = "https://square.site/appointments/buyer/widget/vp26zekhc1y452/L91FW9Z6QEKDK.js";
         script.async = true;
 
-        document.body.appendChild(script);
+        var body = document.getElementsByClassName('book')
+        body.appendChild(script);
 
         return () => {
-            document.body.removeChild(script);
+            body.removeChild(script);
         }
     }, [url]);
 }

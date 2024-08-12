ad51e09620ae6408b90a805aa62d8d187308f4b4
Eo Soo Kim <eosookim@gmail.com>
2024-08-11 23:54:17 -0400

    styling changes on book page

--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -1,40 +1,19 @@
 import "./Book.scss";
-import ToBooking from "../../components/ToBooking/ToBooking";
-import { useState, useEffect } from "react";
-// import { postscribe } from "postscribe";
+import { useEffect } from "react";
 
 export default function Book() {
-  const [book, setBook] = useState(false);
-  const [script, setScript] = useState(null);
-  // useEffect(() => {
-  //   const script = document.createElement("script");
-
-  //   script.src =
-  //     "https://square.site/appointments/buyer/widget/vp26zekhc1y452/L91FW9Z6QEKDK.js";
-  //   script.async = false;
-
-  //   var body = document.getElementsByTagName("main");
-  //   body.appendChild(script);
-
-  //   return () => {
-  //     body.removeChild(script);
-  //   };
-  // }, []);
   useEffect(() => {
     var loadScript = function(src){
       var script = document.createElement('script');
       script.async = false;
       script.src = src;
-      var body = document.getElementsByTagName('main')[0];
+      var body = document.getElementsByClassName('script')[0];
       body.appendChild(script);
     }
 
     loadScript('https://square.site/appointments/buyer/widget/vp26zekhc1y452/L91FW9Z6QEKDK.js');
   }, []);
 
-  useEffect(() => {
-    setBook(false);
-  }, []);
   return (
     <main className="book">
       <div className="book__hero">
@@ -48,6 +27,7 @@ export default function Book() {
           Book Now
         </button>
       </div>
+      <div className="script"></div>
     </main>
   );
 }

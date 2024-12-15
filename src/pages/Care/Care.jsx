eeb10a35f77bcd88b884019e56248db6a3cdec2f
Eo Soo Kim <eosookim@gmail.com>
2024-12-14 23:54:07 -0500

    styling changes to care page

--- src/pages/Care/Care.jsx
+++ src/pages/Care/Care.jsx
@@ -6,6 +6,7 @@ export default function Care() {
   return (
     <main>
       <div className="care-sections">
+        <h1 className="care__title--main">Care for your Lashes</h1>
         <PreCare />
         <AfterCare />
       </div>

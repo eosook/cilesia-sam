d93991a1844a35b947a7c72677147b47c02d5d3d
Eo Soo Kim <eosookim@gmail.com>
2024-12-23 11:19:38 -0500

    changes review section, properly wraps in mobile view

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,12 +1,16 @@
 import "./Reviews.scss";
-import reviewsImage from "../../assets/images/Web Reviews - BLANK.png";
+import reviewsImage from "../../assets/images/What others are saying - BLANK.png";
+import star from "../../assets/images/star.png";
+import { useState } from "react";
 
 export default function Reviews() {
+  const isMobile = window.innerWidth <= 768;
+  const [reviewCount, setReviewCount] = useState(1);
   return (
     <div className="reviews">
-      <img src={reviewsImage} className="reviews__image"></img>
       <div className="reviews__section">
-        <div className="review">
+        <h2 className="reviews__header">What Others are Saying</h2>
+        <div id="review-1" className="review">
           <div className="review__comment">
             A Lovely experience! Sam is very talented and made me feel super
             comfortable during my appointment. Sam is meticulous and attentive,
@@ -14,10 +18,17 @@ export default function Reviews() {
             what I wanted. I highly recommend Sam to anyone looking for
             top-notch lash services. I'd give her ten stars if I could!!! ^-^
           </div>
+          <div className="review__stars">
+            <img src={star}></img>
+            <img src={star}></img>
+            <img src={star}></img>
+            <img src={star}></img>
+            <img src={star}></img>
+          </div>
           <div className="review__name">Georgia</div>
           <div className="review__style">Wispy Volume Lash Extensions</div>
         </div>
-        <div className="review">
+        <div id="review-2" className="review">
           <div className="review__comment">
             Love my lashes. The service I received at Cilesia Beauty Bar
             exceeded all my expectations. I loved everything about my service
@@ -26,10 +37,17 @@ export default function Reviews() {
             I am so happy with the resuly. I love my Hybrid Wispy Lashes. I
             can't wait to try a Volume set.
           </div>
+          <div className="review__stars">
+            <img src={star}></img>
+            <img src={star}></img>
+            <img src={star}></img>
+            <img src={star}></img>
+            <img src={star}></img>
+          </div>
           <div className="review__name">Rose</div>
           <div className="review__style">Wispy Hybrid Lash Extensions</div>
         </div>
-        <div className="review">
+        <div id="review-3" className="review">
           <div className="review__comment">
             Where do I even start. I've been getting lashes for years but I've
             never felt like someone truly understood Asian eyes like Sam does.
@@ -37,6 +55,13 @@ export default function Reviews() {
             to. It takes me an hour to drive to her but it is soo worth it. I
             always feel taken care of, plus the leg wedge and blanket make it
             really comfortable to fall asleep.
+          </div>
+          <div className="review__stars">
+            <img src={star}></img>
+            <img src={star}></img>
+            <img src={star}></img>
+            <img src={star}></img>
+            <img src={star}></img>
           </div>
           <div className="review__name">Cynthia</div>
           <div className="review__style">Anime Lash Extensions</div>

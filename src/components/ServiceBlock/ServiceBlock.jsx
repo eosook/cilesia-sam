2e49d37a5834041913afd94e5242edeb93bd2edd
Eo Soo Kim <eosookim@gmail.com>
2025-01-11 00:30:17 -0500

    pictures no longer stretches in services page, small styling changes in FAQ and services pages

--- src/components/ServiceBlock/ServiceBlock.jsx
+++ src/components/ServiceBlock/ServiceBlock.jsx
@@ -1,14 +1,46 @@
 import "./ServiceBlock.scss";
+import { useState, useEffect } from "react";
 
 export default function ServiceBlock({ side, title, body, image }) {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  if (isMobile){
+    return (
+      <section className="service-block service-block-left">
+        <div className="service-block__image--container">
+          <img
+            className="service-block__image"
+            src={image}
+            alt="service image"
+          ></img>
+        </div>
+        <div className="service-block__text">
+          <h2 className="service-block__title">{title}</h2>
+          <p className="service-block__body">{body}</p>
+        </div>
+      </section>
+    );
+  }
+
   if (side == "left") {
     return (
       <section className="service-block service-block-left">
-        <img
-          className="service-block__image"
-          src={image}
-          alt="service image"
-        ></img>
+        <div className="service-block__image--container">
+          <img
+            className="service-block__image"
+            src={image}
+            alt="service image"
+          ></img>
+        </div>
         <div className="service-block__text">
           <h2 className="service-block__title">{title}</h2>
           <p className="service-block__body">{body}</p>
@@ -22,11 +54,13 @@ export default function ServiceBlock({ side, title, body, image }) {
           <h2 className="service-block__title">{title}</h2>
           <p className="service-block__body">{body}</p>
         </div>
-        <img
-          className="service-block__image"
-          src={image}
-          alt="service image"
-        ></img>
+        <div className="service-block__image--container">
+          <img
+            className="service-block__image"
+            src={image}
+            alt="service image"
+          ></img>
+        </div>
       </section>
     );
   }

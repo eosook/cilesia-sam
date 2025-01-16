6e3fff23641aca6bfe14905fc24a19e24a6ee31a
Eo Soo Kim <eosookim@gmail.com>
2025-01-16 00:02:54 -0500

    removes address from form, small styling changes

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -27,13 +27,6 @@ export default function ContactForm() {
       <input className="contact-form__input" type="text" placeholder="Name" name="user_name" required></input>
       <input className="contact-form__input" type="text" placeholder="Phone" name="phone" required></input>
       <input className="contact-form__input" type="text" placeholder="Email" name="user_email" required></input>
-      <input
-        className="contact-form__input"
-        type="text"
-        placeholder="Address"
-        name="address"
-        required
-      ></input>
       <textarea
         className="contact-form__input contact-form__textarea"
         placeholder="Message"

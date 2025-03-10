f6ffaad55c47957102e3850064456c87c572e0ef
Eo Soo Kim <eosookim@gmail.com>
2025-03-10 13:15:20 -0400

    add public folder, modify vite config to include fallback for SPA routing

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    historyApiFallback: true,
+    open: true,
   },
+  build: {
+    outDir: "dist",
+  },
+  base: "/",
 })
